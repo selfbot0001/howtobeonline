c088d30e81c3b42068df8ed8a1f0199614a7d506
selfbot0001 <44607800+selfbot0001@users.noreply.github.com>
2018-10-30 15:44:18 +0200

    Delete bot.js

--- bot.js
+++
@@ -1,9 +0,0 @@
-const Discord = require("discord.js");
-const self = new Discord.Client();
-
-self.on("ready", () => {
-
-  console.log("Sunt gata sa fiu online 24/7.\n[LOGGER] Sunt logat pe " + self.user.tag)
-});
-
-self.login(process.env.TOKEN);
