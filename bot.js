aa5c1e0a0180275805d1e6b9d91b9de040561fb3
selfbot0001 <44607800+selfbot0001@users.noreply.github.com>
2018-10-30 15:27:30 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,6 @@ const self = new Discord.Client();
 self.on("ready", () => {
 
   console.log("Sunt gata sa fiu online 24/7.\n[LOGGER] Sunt logat pe " + self.user.tag)
-  self.user.setActivity("halloween.exe", { type: "Playing", url: "https://twitch.tv/discordapp" });
 });
 
 self.login(process.env.TOKEN);
